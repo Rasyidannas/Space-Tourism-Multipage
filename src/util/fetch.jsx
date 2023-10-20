c67b509053a06ea36f5c0304286ab6cfcfe0d239
Rasyid Annas <61853270+Rasyidannas@users.noreply.github.com>
2023-10-20 19:21:09 +0700

    fix: api update fetch.jsx

--- src/util/fetch.jsx
+++ src/util/fetch.jsx
@@ -1,6 +1,6 @@
 //load data.json
 export async function loader() {
-  const response = await fetch("../../data.json");
+  const response = await fetch("data.json");
   const data = await response.json();
 
   return data;
